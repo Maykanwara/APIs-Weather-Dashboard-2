bbb6f4dfe3d71da6a737cda7dcabd33c95a1f30b
Kanwara Moore <Maykanwara@gmail.com>
2021-11-11 12:02:37 -0800

    updated APIs-Weather-Dashboard-2

--- script.js
+++ script.js
@@ -14,25 +14,24 @@ function GetInfo(){
     const newName= document.getElementById("cityInput");
     const cityName= document.getElementById("cityName");
     cityName.innerHTML ="--"+newName.value+"--"
-}
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=1469af63617ec2a2f93918eb6da3efe8")
-.then(response) => response.json())
+console.log(newName)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newName.value}+&appid=1469af63617ec2a2f93918eb6da3efe8`)
+.then(response => response.json())
 .then(data =>{
+    console.log(data)
     for(i=0;i<5;i++){
         document.getElementById("day" +(i+1)+"min").innerHTML ="min:" +Number(data.list[i].main.temp_min -283.54).toFixed(1)+"°";
 
     }
     for(i=0;i<5;i++){
-        document.getElementById("day" +(i+1)+"min").innerHTML ="max:" +Number(data.list[i].main.temp_max -286.33).toFixed(1)+"°";
+        document.getElementById("day" +(i+1)+"max").innerHTML ="max:" +Number(data.list[i].main.temp_max -286.33).toFixed(1)+"°";
 }
 
     for(i=0;i<5;i++){
-        document.getElementById("img" +(i+1)).src =" http://openweathermap.org/img/wn/" + data.list[i].weather[0.icon+".jpeg;
+        document.getElementById("img" +(i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".jpeg"
     }
-})
-
-catch(err => alert("Something Went Wrong"))
+}).catch(err => alert("Something Went Wrong"))
 }
 
 
